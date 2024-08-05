0ff8e5e293e9af897ab4aace8f35d8a4d980943e
almiluk <almiluk@gmail.com>
2024-08-05 15:06:42 +0300

    replace digitalWrite with analogWrite for DRIVER2WIRE_PWM

--- src/GyverMotor2.h
+++ src/GyverMotor2.h
@@ -140,11 +140,11 @@ class GMotor2 {
 
             case DRIVER2WIRE_PWM:
                 if (dir > 0) {
-                    digitalWrite(pinA, 0);
+                    analogWrite(pinA, 0);
                     analogWrite(pinB, sp);
                 } else {
                     analogWrite(pinA, sp);
-                    digitalWrite(pinB, 0);
+                    analogWrite(pinB, 0);
                 }
                 break;
 
@@ -163,9 +163,16 @@ class GMotor2 {
 
     // установить все пины
     void setAll(uint8_t val) {
-        digitalWrite(pinA, val);
-        digitalWrite(pinB, val);
-        if (pinC != 255) digitalWrite(pinC, val);
+        if (GM_TYPE == DRIVER2WIRE_PWM){
+            if (val > 0) val = getMax();
+            
+            analogWrite(pinA, val);
+            analogWrite(pinB, val);
+        } else {
+            digitalWrite(pinA, val);
+            digitalWrite(pinB, val);
+            if (pinC != 255) digitalWrite(pinC, val);
+        }
         dir = 0;
     }
 
